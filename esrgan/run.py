66b15ddf55903270d67ee87d2cf496f7d02048bf
Really_true_dude <andrekor2004@gmail.com>
2024-04-10 11:39:28 +0300

    -

--- esrgan/run.py
+++ esrgan/run.py
@@ -1,14 +1,21 @@
 import torch
 from torchvision.utils import save_image
-from model import Generator, initialize_weights
 import numpy as np
 from PIL import Image
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from model import Generator, initialize_weights
+from model_converter import convert_model_weights
 
-def load_model(checkpoint_path: str, model: torch.nn.Module):
-    checkpoint = torch.load(checkpoint_path, map_location=DEVICE, weights_only = False)
-    model.load_state_dict(checkpoint["state_dict"], strict = True)
+def load_model(checkpoint_path: str, model: torch.nn.Module, device = "cpu"):
+    if checkpoint_path == "esrgan/weights/R-ESRGAN_x4.pth":
+        checkpoint = convert_model_weights(checkpoint_path, device=device)
+    elif checkpoint_path == "esrgan/weights/ESRGAN_gen.pth":
+        checkpoint = torch.load(checkpoint_path, map_location = device, weights_only = False)
+    else:
+        raise Exception("Unsupported model dict.. Try downloading one of the models provided on git repository..")
+    
+    model.load_state_dict(checkpoint["params_ema"], strict = True)
 
 def upscale(save_path: str, gen: torch.nn.Module, image_path: str, device):
 
@@ -28,15 +35,17 @@ def upscale(save_path: str, gen: torch.nn.Module, image_path: str, device):
 
 if __name__ == "__main__":
 
-    SAVE_PATH = "output/"
-    ALLOW_CUDA = True
+    SAVE_PATH = "esrgan/output/"
+    IMAGE_PATH = "esrgan/01.jpg"
+    ALLOW_CUDA = False
     DEVICE = "cuda" if torch.cuda.is_available() and ALLOW_CUDA else "cpu"
-    MODEL_PATH = "ESRGAN_gen.pth"
+    MODEL_PATH = "esrgan/weights/R-ESRGAN_x4.pth"
+
 
     gen = Generator(in_channels=3).to(DEVICE)
 
     initialize_weights(gen)
-    load_model(MODEL_PATH, gen)
+    load_model(MODEL_PATH, gen, DEVICE)
 
-    upscale(SAVE_PATH, gen, "img.jpg", DEVICE)
+    upscale(SAVE_PATH, gen, IMAGE_PATH, DEVICE)
     
